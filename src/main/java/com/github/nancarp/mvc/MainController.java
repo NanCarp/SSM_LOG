976215c14d33c92a7e2329835c5197ae85100929
NanCarp <nancarpli@163.com>
2017-08-17 10:16:37 +0800

    1

---
+++ src/main/java/com/github/nancarp/mvc/MainController.java
@@ -0,0 +1,17 @@
+package com.github.nancarp.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by NanCarp on 2017/8/16.
+ */
+@Controller
+@RequestMapping("/mvc")
+public class MainController {
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login() {
+        return "login";
+    }
+}
